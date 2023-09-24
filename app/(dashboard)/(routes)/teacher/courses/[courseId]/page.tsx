b8357685b83ccde81b8b90d9afb28831e70beeff
ahoang2502 <athoang2502@gmail.com>
2023-09-24 13:59:57 +1000

    chapter-create

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,9 @@ import TitleForm from "./_components/TitleForm";
 import DescriptionForm from "./_components/DescriptionForm";
 import ImageForm from "./_components/ImageForm";
 import CategoryForm from "./_components/CategoryForm";
+import PriceForm from "./_components/PriceForm";
+import AttachmentForm from "./_components/AttachmentForm";
+import ChaptersForm from "./_components/ChaptersForm";
 
 import {
 	CircleDollarSign,
@@ -13,8 +16,6 @@ import {
 } from "lucide-react";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import PriceForm from "./_components/PriceForm";
-import AttachmentForm from "./_components/AttachmentForm";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -24,11 +25,17 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const course = await db.course.findUnique({
 		where: {
 			id: params.courseId,
+			userId,
 		},
 		include: {
 			attachments: {
 				orderBy: {
 					createdAt: "desc",
+				},
+			},
+			chapters: {
+				orderBy: {
+					position: "asc",
 				},
 			},
 		},
@@ -48,6 +55,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 		course.imageUrl,
 		course.price,
 		course.categoryId,
+		course.chapters.some((chapter) => chapter.isPublished),
 	];
 
 	const totalFields = requiredFields.length;
@@ -94,7 +102,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 							<h2 className="text-xl">Course chapters</h2>
 						</div>
 
-						<div className="">TODO: Chapters</div>
+						<ChaptersForm initialData={course} courseId={course.id} />
 					</div>
 
 					<div className="">
